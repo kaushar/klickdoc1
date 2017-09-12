dd389259ad34d650a488fd4691ad1978cd01055d
kaushar Alam <akaushar@gmail.com>
2017-09-12 14:55:58 +0530

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo 'Welcome to klickdoc<br><br>';
-echo 'Testing Git<br><br>';
-echo 'Its Working <br><br>';
-echo 'Step 1  <br><br>';
-echo 'Smart Git Check  <br><br>';
-echo 'Smart Git Check STEP 2 <br><br>';
-echo 'Smart Git Check STEP 3 <br><br>';
-?>
